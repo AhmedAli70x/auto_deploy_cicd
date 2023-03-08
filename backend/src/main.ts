694631adb0b5677ef7e4a28e2b6009bc5967102b
Ahmed <ahmedali70x@mgil.com>
2023-03-08 15:02:46 +0000

    run-migrations added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cors from 'cors';
-import * as express from 'express';
-import { AppLogger } from './modules/app/app.logger';
-import { AppModule } from './modules/app/app.module';
-import { TransformInterceptor } from './modules/common/interceptors/TransformInterceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from './modules/config/config.service';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './modules/app/app.module';
+import { AppLogger } from './modules/app/app.logger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as cors from 'cors';
+import { TransformInterceptor } from './modules/common/interceptors/TransformInterceptor';
+import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
 async function bootstrap() {
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
@@ -45,7 +45,7 @@ async function bootstrap() {
       }
     },
   };
-  app.use(cors(corsOptions));
+  app.use(cors({  "origin": "*", "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"}));
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
