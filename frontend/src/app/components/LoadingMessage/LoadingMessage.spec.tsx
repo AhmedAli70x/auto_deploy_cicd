5c3f9e6c43d2f9529a766395640ff9aace910918
Ahmed <ahmedali70x@mgil.com>
2023-03-09 16:16:30 +0000

    run migrations

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -1,6 +1,6 @@
-import { LoadingMessage } from 'app/components/LoadingMessage';
-import { shallow } from 'enzyme';
 import * as React from 'react';
+import { shallow } from 'enzyme';
+import { LoadingMessage } from 'app/components/LoadingMessage';
 
 describe('<LoadingMessage>', () => {
   describe('Props', () => {
@@ -8,7 +8,7 @@ describe('<LoadingMessage>', () => {
       it('Should render the props message', () => {
         const message = 'Hello!';
         const wrapper = shallow(<LoadingMessage message={message} />);
-        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy(); 
+        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy();
       });
     });
   });
